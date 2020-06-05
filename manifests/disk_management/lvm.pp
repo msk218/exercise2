00215a0cad53ee0cb7ec9069b326bd2d88711c73
Milton Kim <miltonkim@Miltons-MBP.attlocal.net>
2020-06-04 20:05:44 -0500

    lvm

--- manifests/disk_management/lvm.pp
+++ manifests/disk_management/lvm.pp
@@ -2,5 +2,19 @@
 #
 #
 class execise2::disk_management::lvm {
-	# resources
-}
+	# Create a hash from Hiera Data with the Users
+	$physical_volume = hiera('exercise2::physical_volume', {})
+	# With Create Resource Converts a hash into a set of resources
+	create_resources(physical_volume, $physical_volume)
+	# Create a hash from Hiera Data with the Users
+	$volume_group = hiera('exercise2::volume_group', {})
+	# With Create Resource Converts a hash into a set of resources
+	create_resources(volume_group, $volume_group)
+	# Create a hash from Hiera Data with the Users
+	$logical_volume = hiera('exercise2::logical_volume', {})
+	# With Create Resource Converts a hash into a set of resources
+	create_resources(logical_volume, $logical_volume)
+	# Create a hash from Hiera Data with the Users
+	$filesystem = hiera('exercise2::filesystem', {})
+	# With Create Resource Converts a hash into a set of resources
+	create_resources(filesystem, $filesystem)
